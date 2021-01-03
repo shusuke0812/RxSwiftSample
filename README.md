7408011303ea23bfe848204f43033c20ff44d38c
shusuke0812 <shu.ota0812@gmail.com>
2021-01-03 22:54:15 +0900

    [add]プロジェクトファイル追加

---
+++ README.md
@@ -0,0 +1,19 @@
+RxSwiftを使ったサンプルコード
+====
+　
+## 開発環境
+- Xcode version 12
+- pod version 1.9.1
+- MacOS Catalina version 10.15.4
+
+## 概要
+### 背景と目的
+- 
+
+### アプリの要件
+- 
+
+## 参考文献
+- 
+
+
---
+++ RxSwiftSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftSample
+//
+//  Created by Shusuke Ota on 2021/1/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
