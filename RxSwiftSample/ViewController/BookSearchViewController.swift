d1a3a927654fbb9d709ed11276efab1ec7403582
shusuke0812 <shu.ota0812@gmail.com>
2021-01-16 16:30:29 +0900

    [rename]トップ画面のViewController名とStoryboard名を変更

--- RxSwiftSample/ViewController/BookSearchViewController.swift
+++ RxSwiftSample/ViewController/BookSearchViewController.swift
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class BookSearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
