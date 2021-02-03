234186a1e6a302d3c3b74d7ab987f61ab1716be9
shusuke0812 <shu.ota0812@gmail.com>
2021-02-03 17:12:18 +0900

    [add]整数のフォーマット（偶数、奇数など）を判定するHelperクラスを追加

---
+++ RxSwiftSample/Helper/NumberFormHelper.swift
@@ -0,0 +1,31 @@
+//
+//  NumberFormHelper.swift
+//  RxSwiftSample
+//
+//  Created by Shusuke Ota on 2021/2/3.
+//
+
+import Foundation
+
+class NumberFormHelper {
+    /// 偶数・奇数を判定する
+    /// - Parameter numer: 判定する整数
+    /// - Returns: 偶数（true）、奇数（false）
+    internal func judgeEvenOrOdd(number: Int) -> Bool {
+        if number % 2 == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    /// `3n - 2`で表される順列に含まれる整数かどうかを判定する
+    /// - Parameter number: 判定する整数
+    internal func judgePermutation(number: Int) -> Bool {
+        let judgeNumber = (number + 2) % 3
+        if judgeNumber == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+}
