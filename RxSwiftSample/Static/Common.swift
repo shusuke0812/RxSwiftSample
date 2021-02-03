97f3760e8414693aca6d6e32e1a60afbef666f09
shusuke0812 <shu.ota0812@gmail.com>
2021-02-03 18:36:13 +0900

    [add]ネットワークエラーの列挙型を追加

--- RxSwiftSample/Static/Common.swift
+++ RxSwiftSample/Static/Common.swift
@@ -14,3 +14,16 @@ enum HTTPMethod: String {
     case put    = "PUT"
     case delete = "DELETE"
 }
+/// ネットワークエラー
+enum NetworkError: Error {
+    case invalidUrl         // 不正なURL
+    case invalidResponse    // 不正なレスポンス
+    case unknown            // 想定外エラー
+    func description() -> String {
+        switch self {
+        case .invalidUrl:       return "DEBUG： 不正なURLです"
+        case .invalidResponse:  return "DEBUG： 不正なレスポンスです"
+        case .unknown:          return "DEBUG： レスポンスに失敗しました"
+        }
+    }
+}
