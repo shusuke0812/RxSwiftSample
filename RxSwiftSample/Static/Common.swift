3ec83b3692ef45f4528a9e6ed1c71e986bd26689
shusuke0812 <shu.ota0812@gmail.com>
2021-02-03 16:19:08 +0900

    [add]Httpメソッドの列挙型を追加

---
+++ RxSwiftSample/Static/Common.swift
@@ -0,0 +1,16 @@
+//
+//  Common.swift
+//  RxSwiftSample
+//
+//  Created by Shusuke Ota on 2021/2/3.
+//
+
+import Foundation
+
+/// HTTPメソッド
+enum HTTPMethod: String {
+    case get    = "GET"
+    case post   = "POST"
+    case put    = "PUT"
+    case delete = "DELETE"
+}
