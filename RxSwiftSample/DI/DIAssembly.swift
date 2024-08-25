bfe7d3591c4e4fe3a2ccdb17771c5b5029ce5f66
shusuke0812 <shu.ota0812@gmail.com>
2024-08-25 16:10:33 +0900

    fix: DIAssembly

--- RxSwiftSample/DI/DIAssembly.swift
+++ RxSwiftSample/DI/DIAssembly.swift
@@ -6,21 +6,41 @@
 //
 
 import Swinject
+import SwinjectStoryboard
 
-class RepositoryAssembly: Assembly {
-    func assemble(container: Container) {
-        container.register(PhotoSearchRepositoryProtocol.self) { _ in
-            PhotoSearchRepository()
+extension SwinjectStoryboard {
+    class RepositoryAssembly: Assembly {
+        func assemble(container: Container) {
+            container.register(PhotoSearchRepositoryProtocol.self) { _ in
+                PhotoSearchRepository()
+            }
+            .inObjectScope(.container) // Singleton
         }
-        .inObjectScope(.container) // Singleton
     }
-}
 
-class ViewModelAssembly: Assembly {
-    func assemble(container: Container) {
-        container.register(PhotoSearchViewModel.self) { resolver in
-            let repository = resolver.resolve(PhotoSearchRepository.self)!
-            return PhotoSearchViewModel(photoSearchRepository: repository)
+    class ViewModelAssembly: Assembly {
+        func assemble(container: Container) {
+            container.register(PhotoSearchViewModel.self) { resolver in
+                let repository = resolver.resolve(PhotoSearchRepositoryProtocol.self)!
+                return PhotoSearchViewModel(photoSearchRepository: repository)
+            }
         }
+    }
+    
+    class ViewControllerAssembly: Assembly {
+        func assemble(container: Container) {
+            container.storyboardInitCompleted(PhotoSearchViewController.self) { resolver, vc in
+                vc.viewModel = resolver.resolve(PhotoSearchViewModel.self)!
+            }
+        }
+    }
+    
+    class func setup() {
+        let assembler = Assembler(container: SwinjectStoryboard.defaultContainer)
+        assembler.apply(assemblies: [
+            RepositoryAssembly(),
+            ViewModelAssembly(),
+            ViewControllerAssembly()
+        ])
     }
 }
--- RxSwiftSample/Screen/PhotoSearch/ViewController/PhotoSearchViewController.swift
+++ RxSwiftSample/Screen/PhotoSearch/ViewController/PhotoSearchViewController.swift
@@ -16,15 +16,13 @@ import RxSwift
 
 class PhotoSearchViewController: UIViewController {
     private var baseView: PhotoSearchBaseView { view as! PhotoSearchBaseView }
-    private var viewModel: PhotoSearchViewModel!
+    var viewModel: PhotoSearchViewModel!
 
     private let disposeBag = RxSwift.DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // MARK: - 初期設定
-        // ViewModel初期化
-        viewModel = PhotoSearchViewModel(photoSearchRepository: PhotoSearchRepository())
         // Delegate・DataSource設定
         baseView.collectionView.rx.setDelegate(self).disposed(by: disposeBag)
         baseView.searchBar.rx.setDelegate(self).disposed(by: disposeBag)
