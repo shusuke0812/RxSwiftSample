251b435f4788f22c5a635e54aecf0953c2d36b58
shusuke0812 <shu.ota0812@gmail.com>
2024-08-25 18:12:17 +0900

    add: saveSelectedPhoto()とgetSelectedPhoto()をPhotoSearchRepositoryに追加

--- RxSwiftSample/Repository/PhotoSearchRepository.swift
+++ RxSwiftSample/Repository/PhotoSearchRepository.swift
@@ -14,9 +14,12 @@ protocol PhotoSearchRepositoryProtocol {
     ///   - searchWord: 検索ワード
     ///   - completion: 成功、失敗ハンドル
     func getPhotos(searchWord: String) -> Observable<[Photo]>
+    func saveSelectedPhoto(index: Int)
+    func getSelectedPhoto() -> Observable<Photo>
 }
 class PhotoSearchRepository: PhotoSearchRepositoryProtocol {
     private var photos: [Photo] = []
+    private var selectedPhoto: Photo?
 }
 // MARK: - API Method
 extension PhotoSearchRepository {
@@ -58,6 +61,23 @@ extension PhotoSearchRepository {
             task.resume()
             
             return Disposables.create { task.cancel() }
+        }
+    }
+    
+    func saveSelectedPhoto(index: Int) {
+        selectedPhoto = photos[index]
+    }
+    
+    func getSelectedPhoto() -> Observable<Photo> {
+        Observable<Photo>.create { observer in
+            guard let selectedPhoto = self.selectedPhoto else {
+                observer.on(.error(APIClientError.unknown)) // TODO: 別のエラー型に置き換える
+                return Disposables.create()
+            }
+            observer.on(.next(selectedPhoto))
+            observer.on(.completed)
+            
+            return Disposables.create()
         }
     }
 }
